6039231123f8c4ee3e17dddee8d8b5b59d479129
RichardTry <bazrovstepan@gmail.com>
2020-10-21 17:10:18 +0300

    Punching and spectator

--- mods/among_us/init.lua
+++ mods/among_us/init.lua
@@ -3,16 +3,20 @@ local modpath = minetest.get_modpath(modname)
 
 math.randomseed(os.time())
 
-among_us = {impostors = 1, impostors_list = {}, game_spawn = {x = 8, y = 21.5, z = 4}, game_time = false, guess_time = true, players_list = {}}
+among_us = {impostors = 1, impostors_list = {}, skins = {}, game_spawn = {x = 8, y = 21.5, z = 4}, game_time = false, guess_time = true, players_list = {}}
 
 local players = 0
 minetest.register_on_joinplayer(function(player)
 	players = players + 1
 	
+	player:set_nametag_attributes({text = " "})
+	
+	among_us.skins[player] = players
 	default.player_set_textures(player, {"skin_"..players..".png"})
 	local meta = player:get_meta()
 	meta:set_string("among_us:", skins.skins[playername])
 end)
+
 minetest.register_on_leaveplayer(function(player)
 	players = players - 1
 end)
@@ -28,7 +32,6 @@ function among_us.start_game()
 	for i=1,among_us.impostors do
 		while true do
 			local rand = math.random(1, players)
-			minetest.chat_send_all("try "..rand)
 			if impostors_numbers[rand] == nil then
 				impostors_numbers[rand] = true
 				break
@@ -41,8 +44,9 @@ function among_us.start_game()
 		player:set_pos({x = among_us.game_spawn.x + math.cos(deg) * 2, y = among_us.game_spawn.y, z = among_us.game_spawn.z + math.sin(deg) * 2})
 		player:set_look_vertical(0)
 		player:set_look_horizontal(deg + math.pi / 2)
-		if impostors_numbers[rand] ~= nil then
-			minetest.chat_send_all(player:get_name().." is an impostor!")
+		if impostors_numbers[i] ~= nil then
+			minetest.chat_send_all(player:get_player_name().." is an impostor!")
+			among_us.impostors_list[player] = true
 		end
 	end		
 	start_timer = false
@@ -64,6 +68,54 @@ minetest.register_globalstep(function(dtime)
 	end
 end)
 
+
+function among_us.guess_formspec(name)
+
+	local formspec = ""
+
+	--if skins.invplus then
+	--	formspec = "size[8,8.6]"
+	--		.. "bgcolor[#08080822;true]"
+	--end
+
+	formspec = formspec .. "label[.5,2;" .. "Select Player Skin:" .. "]"
+		.. "textlist[.5,2.5;6.8,6;skins_set;"
+
+	local meta
+	local selected = 1
+
+	for i = 1, players do
+
+		formspec = formspec .. among_us.meta[i].name
+
+		if skins.skins[name] == among_us.skins[i] then
+			selected = i
+			meta = skins.meta[ skins.skins[name] ]
+		end
+
+		if i < #skins.list then
+			formspec = formspec ..","
+		end
+	end
+
+	--if skins.invplus then
+	--	formspec = formspec .. ";" .. selected .. ";true]"
+	--else
+	--	formspec = formspec .. ";" .. selected .. ";false]"
+	--end
+
+	if meta then
+		if meta.name then
+			formspec = formspec .. "label[2,.5;" .. "Name: " .. meta.name .. "]"
+		end
+		if meta.author then
+			formspec = formspec .. "label[2,1;" .. "Author: " .. meta.author .. "]"
+		end
+	end
+
+	return formspec
+end
+
 function among_us.get_formspec(name)
     -- TODO: display whether the last guess was higher or lower
 
@@ -79,7 +131,7 @@ function among_us.get_formspec(name)
 end
 
 function among_us.show_to(name)
-    minetest.show_formspec(name, "among_us:settings", among_us.get_formspec(name))
+    minetest.show_formspec(name, "among_us:settings", among_us.guess_formspec(name))
 end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
@@ -95,11 +147,11 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
     end
 end)
 
---minetest.register_chatcommand("game", {
---    func = function(name)
---        among_us.show_to(name)
---    end,
---})
+minetest.register_chatcommand("game", {
+    func = function(name)
+        among_us.show_to(name)
+    end,
+})
 
 minetest.register_chatcommand("impostors", {
     params = "<number>",
@@ -140,6 +192,35 @@ minetest.register_chatcommand("guess", {
         end
     end,
 })
+
+function player_to_spectator(player)
+	local privs = {}
+	privs.fly, privs.fast = true, true
+	privs.interact, privs.shout = nil, nil
+	vs, fs ,cb = { x = 0, y = 0 }, false, { 0, 0, 0, 0, 0, 0 }
+	minetest.set_player_privs(player:get_player_name(), privs)
+	player:set_nametag_attributes({ color = { a = 0, r = 0, g = 0, b = 0 } })
+	player:set_armor_groups({ immortal = 1 })
+	player:hud_set_flags({
+		hotbar = false,
+		healthbar = false,
+		crosshair = false,
+		wielditem = false,
+		breathbar = false,
+	})
+	player:set_properties({
+		visual_size = vs,
+		makes_footstep_sound = fs,
+		collisionbox = cb,
+	})
+end
+
+minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
+	minetest.chat_send_player(player:get_player_name(), "You were killed by "..hitter:get_player_name().."!")
+	player_to_spectator(player)
+	--if among_us.impostors_list[hitter] ~= nil and among_us.impostors_list[player] == nil then
+	--end
+end)
 
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
